aa0a34b5fbbb23d2d7ac20b5adae666408b8f7a5
den shkitun <denniskitun@gmail.com>
2017-10-29 00:07:44 +0300

    Added and registered models.

--- blog_app/admin.py
+++ blog_app/admin.py
@@ -1,6 +1,12 @@
 # -*- coding: utf-8 -*-
+
+# login - admin
+# password - administrator
+
 from __future__ import unicode_literals
-
 from django.contrib import admin
+from .models import Post, Comment, Category
 
-# Register your models here.
+admin.site.register(Category)
+admin.site.register(Post)
+admin.site.register(Comment)
